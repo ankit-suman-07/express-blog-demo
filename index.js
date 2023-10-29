3b9012a6b0f7f129b672422e846ac3a9b52140de
ankit-suman-07 <ankitsuman07@gmail.com>
2023-10-29 21:08:58 +0530

    Demo commit

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require('express')
+const exphbs = require('express-handlebars');
+
+
 const app = express()
 const path = require('path')
 const port = 3002
+
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main', // Optional: specify the default layout file (main.handlebars)
+    extname: '.handlebars'  // Optional: specify the file extension for Handlebars templates
+}));
+app.set('view engine', 'handlebars');
+
 
 app.use(express.static(path.join(__dirname, "static")))
 app.use('/', require(path.join(__dirname, 'routes/blog.js')));
 
 app.listen(port, () => {
-    console.log(`Blog App listening on port http://localhost${port}`)
+    console.log(`Blog App listening on port http://localhost:${port}`)
 })
